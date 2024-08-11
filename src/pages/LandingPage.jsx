f4f5f699efc31a8926d9d75cb0c8e85225d860a7
Chinmay <chinmaybarik214@gmail.com>
2024-08-11 13:22:38 +0530

    made cutout wo border

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -13,13 +13,15 @@ function LandingPage() {
         <p className='main__content main__content--bold'>NeuralThread</p>
         <p className='main__content'>Unleashing the Future of Textile Design with</p>
         <p className='main__content main__content--semiBold'>AI Pattern Generation</p>
-        
+
         <div className='main__form'>
           <p className='form__header'>SEND MESSAGE</p>
-          <input className='form__input form__input--left' placeholder='Your Name'/>
-          <input className='form__input form__input--right' placeholder='Phone Number'/>
-          <input className='form__input' placeholder='Email id'/>
-          <textarea className='form__input form__msg' placeholder='Message'/>
+          <input className='form__input form__input--left' placeholder='Your Name' />
+          <input className='form__input form__input--right' placeholder='Phone Number' />
+          <input className='form__input' placeholder='Email id' />
+          <textarea className='form__input form__msg' placeholder='Message' />
+          <button className='form__cutout'>SEND</button>  
+          <button className='form__submit' type='submit'>SEND</button>
         </div>
       </div>
       <div className="hero__imgs">def</div>
